2588be551fb84e33ee7e5a3815ef34e8f0a3b3db
Raymon85 <85rayon@gafe.molndal.se>
2026-01-18 14:35:49 +0100

    change api

--- BudgetTracker/BudgetTracker.Core/obj/Debug/net10.0/BudgetTracker.Core.AssemblyInfo.cs
+++ BudgetTracker/BudgetTracker.Core/obj/Debug/net10.0/BudgetTracker.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4325fd347a27501b743422f6965c375c6df1203c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a053b20c222f6c798d94eda9bbe63bf5a53079")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
